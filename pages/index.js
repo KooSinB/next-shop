68ae43414612e904b4eff059d3ba9d54b2eadd59
“KooSinB <khiyaj0320@gmail.com>
2022-09-26 12:45:16 +0900

    ADD 블라블라

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ import data from "../utils/data";
 export default function Home() {
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4
+      xl:grid-cols-5 2xl:grid-cols-6">
         {data.products.map((product) => (
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
