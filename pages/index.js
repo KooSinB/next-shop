970d416b3395f7ce8dcc8de86c9e1b6e0ae14a8b
Ji Young Wook <https://github.com/KooSinB/webaap>
2022-10-11 22:04:50 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import Layout from "../components/Layout";
-import ProductItem from "../components/Productitem";
-import styles from "../styles/Home.module.css";
 import data from "../utils/data";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
 
 export default function Home() {
   return (
-    <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4
-      xl:grid-cols-5 2xl:grid-cols-6">
+    <Layout title="Home">
+      <div className="grid grid-cols1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {data.products.map((product) => (
-          <ProductItem product={product} key={product.slug}></ProductItem>
+          <ProductItem product={product} key={product.slug} />
         ))}
       </div>
     </Layout>
