4f6aaa5958a99d1c7be1dcd3b37252ad90ad71c3
JiYoungWook <https://github.com/KooSinB/next-shop>
2022-12-12 00:10:04 +0900

    asdf

--- pages/cart.js
+++ pages/cart.js
@@ -6,8 +6,6 @@ import Layout from '../components/Layout'
 import { Store } from '../utils/Store'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import axios from 'axios'
-import { toast } from 'react-toastify'
 
 function CartScreen() {
   const router = useRouter()
@@ -18,16 +16,10 @@ function CartScreen() {
   const removeItemHandler = (item) => {
     dispatch({ type: 'CART_REMOVE_ITEM', payload: item })
   }
-  const updateCartHandler = async (item, qty) => {
+  const updateCartHandler = (item, qty) => {
     const quantity = Number(qty)
-    const { data } = await axios.get(`/api/products/${item._id}`)
-    if (data.countInStock < quantity) {
-      return toast.error('Sorry. Product is out of stock')
-    }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } })
-    toast.success('Product updated in the cart')
   }
-
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
